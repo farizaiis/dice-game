55e0d81efd7b64720c6eec2064e28f76307257ff
farizaiis <farizaiis@hotmail.com>
2021-11-23 12:17:59 +0700

    modify get random number for dice

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // alert("Working")
 
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomDiceImage = "dice" + randomNumber1 + ".png";
+
+let randomImageSource = "images/" + randomDiceImage
+
+
+let image1 = document.querySelectorAll("img")[0]
+
+image1.setAttribute("src", randomImageSource);
