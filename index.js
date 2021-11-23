14701f1a122b5b20c2104411accea85c44d80ea4
farizaiis <farizaiis@hotmail.com>
2021-11-23 12:25:28 +0700

    modify dice player 1 and add random number for player 2 at indexjs

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // alert("Working")
 
+
+//Player 1 Dice
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-let randomDiceImage = "dice" + randomNumber1 + ".png";
-
-let randomImageSource = "images/" + randomDiceImage
-
+let randomImageSource = "images/dice" + randomNumber1 + ".png"
 
 let image1 = document.querySelectorAll("img")[0]
 
 image1.setAttribute("src", randomImageSource);
+
+
+//Player 2 Dice
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
