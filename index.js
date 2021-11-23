3e73e3d2ea8f7fcaa63e91c85dd8618e6e87d299
farizaiis <farizaiis@hotmail.com>
2021-11-23 17:30:05 +0700

    add start button

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 // alert("Working")
 
+$('.startbutton').click(function () {
+    //Player 1 Dice
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-//Player 1 Dice
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomImageSource = 'images/dice' + randomNumber1 + '.png';
 
-let randomImageSource = "images/dice" + randomNumber1 + ".png"
+    let image1 = document.querySelectorAll('img')[0];
 
-let image1 = document.querySelectorAll("img")[0]
+    image1.setAttribute('src', randomImageSource);
 
-image1.setAttribute("src", randomImageSource);
+    //Player 2 Dice
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+    let randomImageSource2 = 'images/dice' + randomNumber2 + '.png';
 
-//Player 2 Dice
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    document.querySelectorAll('img')[1].setAttribute('src', randomImageSource2);
 
-let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-//Winner condition
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 Wins!"
-}
-if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins!"
-}
-if (randomNumber1 == randomNumber2) {
-    document.querySelector("h1").innerHTML = "DRAW!"
-}
+    //Winner condition
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector('h1').innerHTML = 'Player 1 Wins!';
+    }
+    if (randomNumber1 < randomNumber2) {
+        document.querySelector('h1').innerHTML = 'Player 2 Wins!';
+    }
+    if (randomNumber1 == randomNumber2) {
+        document.querySelector('h1').innerHTML = 'DRAW!';
+    }
+});
