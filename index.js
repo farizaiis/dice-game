f2f3cc8f0db973ec7925d49e906395b4e4a0d121
farizaiis <farizaiis@hotmail.com>
2021-11-23 12:28:18 +0700

    add winner condition

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!"
+}
+
+if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!"
+}
+
+if (randomNumber1 == randomNumber2) {
+    document.querySelector("h1").innerHTML = "DRAW!"
+}
